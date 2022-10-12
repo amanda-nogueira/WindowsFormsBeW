36f8b8ddce47eeacfbeb2439b3736340ba23d244
amand <amand@LAPTOP-9C9688MD>
2022-10-11 23:12:59 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsBeW/ClassLogin.cs
@@ -0,0 +1,19 @@
+﻿public class User
+{
+    //atributos
+    private string user;
+    private string senha;
+    private string nome;
+
+    //contrutor padrão
+
+    //metódos
+    public void SetUser(string p_user) { this.user = p_user; }
+    public string GetUser() { return this.user; }
+
+    public void SetSenha(string p_senha) { this.senha = p_senha; }
+    public string GetSenha() { return this.senha; }
+
+    public void SetNome(string p_nome) { this.nome = p_nome; }
+    public string GetNome() { return this.nome; }
+}
---
+++ WindowsFormsBeW/Login.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsBeW
+{
+    public partial class Login : Form
+    {
+        //Instância do objeto
+        User usuario = new User();
+        Principal princ = new Principal();
+        public Login()
+        {
+            InitializeComponent();
+            usuario.SetNome("Neymar");
+            usuario.SetUser("Ney");
+            usuario.SetSenha("2022");
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((usuario.GetUser() == textBox1.Text) && (usuario.GetSenha() == textBox2.Text))
+            {
+                MessageBox.Show(usuario.GetNome() + "\nAcesso permitido!", "Login", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                MessageBox.Show("Acesso negado!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            this.Hide();
+            princ.Show();
+        }
+    }
+}
